ff19477fb3ddc23ace88d5a65b15956eff3b4fa2
Jacob Gad <jacob.gad@hotmail.com>
2022-01-21 15:44:28 +1100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import SignaturePad from './SignaturePad';
+
+export default function App() {
+	const [signature, setSignature] = useState<string>('');
+
+	return (
+		<div style={{ display: 'grid', placeItems: 'center' }}>
+			<h1>Signature: </h1>
+			<SignaturePad width={500} height={200} setSignature={setSignature} />
+			<h2>Result: </h2>
+			{signature && (
+				<img style={{ border: '3px solid', borderRadius: 10 }} src={signature} alt='Signature' />
+			)}
+		</div>
+	);
+}
---
+++ src/SignaturePad.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import Signature_Pad from 'signature_pad';
+
+interface Props {
+	width: Number;
+	height: Number;
+	setSignature: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function SignaturePad({ width, height, setSignature }: Props) {
+	const [signaturePad, setSignaturePad] = useState<Signature_Pad>();
+
+	function readyPad() {
+		let wrapper = document.getElementById('signature-pad');
+		let canvas = wrapper?.querySelector('canvas');
+		if (canvas) {
+			const ratio = Math.max(window.devicePixelRatio || 1, 1);
+			canvas.width = canvas.offsetWidth * ratio;
+			canvas.height = canvas.offsetHeight * ratio;
+			canvas.getContext('2d')?.scale(ratio, ratio);
+			let readySignaturePad = new Signature_Pad(canvas);
+			setSignaturePad(readySignaturePad);
+		}
+	}
+
+	function clear() {
+		signaturePad && signaturePad.clear();
+		setSignature('');
+	}
+
+	function submit() {
+		signaturePad && setSignature(signaturePad.toDataURL('image/svg+xml'));
+	}
+
+	useEffect(() => {
+		readyPad();
+	}, []);
+
+	return (
+		<>
+			<div
+				id='signature-pad'
+				style={{
+					width: width.toString() + 'px',
+					height: height.toString() + 'px',
+					border: '3px solid',
+					borderRadius: 10,
+				}}
+			>
+				<canvas style={{ width: '100%', height: '100%' }} />
+			</div>
+			<div>
+				<button onClick={clear}>Clear</button>
+				<button onClick={submit}>Submit</button>
+			</div>
+		</>
+	);
+}
