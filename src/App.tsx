b70c930fbfffd5ffef5bb549a37003fcc6e2ebba
Jacob Gad <jacob.gad@hotmail.com>
2022-01-24 12:56:47 +1100

    add example form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,63 @@
 import { useState } from 'react';
 import SignaturePad from './SignaturePad';
 
+interface Form {
+	firstName: string;
+	lastName: string;
+	signature: string;
+}
+
 export default function App() {
+	const [firstName, setFirstName] = useState<string>('');
+	const [lastName, setLastName] = useState<string>('');
 	const [signature, setSignature] = useState<string>('');
+	const [form, setForm] = useState<Form | undefined>(undefined);
+
+	function submit() {
+		setForm({ firstName, lastName, signature });
+	}
 
 	return (
-		<div style={{ display: 'grid', placeItems: 'center' }}>
-			<h1>Signature: </h1>
-			<SignaturePad width={'50%'} height={200} setSignature={setSignature} />
-			<h2>Result: </h2>
-			{signature && (
-				<img style={{ border: '3px solid', borderRadius: 10 }} src={signature} alt='Signature' />
+		<div style={{ display: 'grid', placeItems: 'center', gap: 10 }}>
+			<h1>Signature Form: </h1>
+			<div>
+				<label htmlFor='firstName'>First Name</label>
+				<input
+					type='text'
+					id='firstName'
+					onChange={(e) => {
+						setFirstName(e.target.value);
+					}}
+				/>
+			</div>
+			<div>
+				<label htmlFor='lastName'>Last Name</label>
+				<input
+					type='text'
+					id='lastName'
+					onChange={(e) => {
+						setLastName(e.target.value);
+					}}
+				/>
+			</div>
+			<SignaturePad setSignature={setSignature} />
+			<button onClick={submit}>Submit</button>
+
+			<h2>Live Form Preview</h2>
+			<p>First Name: {firstName}</p>
+			<p>Last Name: {lastName}</p>
+			<textarea style={{ width: 400, height: 150, wordWrap: 'break-word' }} value={signature} />
+
+			<h2>Form Output: </h2>
+			{form && (
+				<>
+					<h3>JSON</h3>
+					<div style={{ width: 500 }}>
+						<pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(form, null, 2)}</pre>
+					</div>
+					<h3>Signature Image</h3>
+					<img src={form.signature} alt='Signature' />
+				</>
 			)}
 		</div>
 	);
--- src/SignaturePad.tsx
+++ src/SignaturePad.tsx
@@ -2,58 +2,51 @@ import { useEffect, useState } from 'react';
 import Signature_Pad from 'signature_pad';
 
 interface Props {
-	width?: number | string;
-	height?: number | string;
 	setSignature: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function SignaturePad({ width, height, setSignature }: Props) {
+export default function SignaturePad({ setSignature }: Props) {
 	const [signaturePad, setSignaturePad] = useState<Signature_Pad>();
 
-	function readyPad() {
-		let wrapper = document.getElementById('signature-pad');
-		let canvas = wrapper?.querySelector('canvas');
-		if (canvas) {
-			const ratio = Math.max(window.devicePixelRatio || 1, 1);
-			canvas.width = canvas.offsetWidth * ratio;
-			canvas.height = canvas.offsetHeight * ratio;
-			canvas.getContext('2d')?.scale(ratio, ratio);
-			let readySignaturePad = new Signature_Pad(canvas);
-			setSignaturePad(readySignaturePad);
-		}
-	}
-
-	function clear() {
+	const clear = () => {
 		signaturePad && signaturePad.clear();
 		setSignature('');
-	}
+	};
 
-	function submit() {
-		signaturePad && setSignature(signaturePad.toDataURL('image/svg+xml'));
+	function update() {
+		signaturePad && setSignature(signaturePad.toDataURL());
 	}
 
 	useEffect(() => {
+		function readyPad() {
+			let wrapper = document.getElementById('signature-pad');
+			let canvas = wrapper?.querySelector('canvas');
+			if (canvas) {
+				const ratio = Math.max(window.devicePixelRatio || 1, 1);
+				canvas.width = canvas.offsetWidth * ratio;
+				canvas.height = canvas.offsetHeight * ratio;
+				canvas.getContext('2d')?.scale(ratio, ratio);
+				let readySignaturePad = new Signature_Pad(canvas);
+				setSignaturePad(readySignaturePad);
+			}
+		}
 		readyPad();
-		if (typeof width == 'number') width = width + 'px';
-		if (typeof height == 'number') height = height + 'px';
-	}, []);
+	}, [setSignaturePad]);
 
 	return (
 		<>
+			<button onClick={clear}>Clear Signature</button>
 			<div
 				id='signature-pad'
 				style={{
-					width,
-					height,
+					width: 350,
+					height: 150,
 					border: '3px solid',
 					borderRadius: 10,
+					backgroundColor: 'white',
 				}}
 			>
-				<canvas style={{ width: '100%', height: '100%' }} />
-			</div>
-			<div>
-				<button onClick={clear}>Clear</button>
-				<button onClick={submit}>Submit</button>
+				<canvas style={{ width: '100%', height: '100%' }} onClick={update} onTouchEnd={update} />
 			</div>
 		</>
 	);
